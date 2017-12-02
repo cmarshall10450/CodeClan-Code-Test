406083901203957b611669dbb8a840d9cbf85cab
Chris Marshall <cmarshall10450@gmail.com>
2017-12-02 10:33:29 +0000

    Added basic setup for shopping basket.

---
+++ src/ShoppingBasket.java
@@ -0,0 +1,15 @@
+import org.junit.Before;
+
+/**
+ * Created by chris on 02/12/2017.
+ */
+public class ShoppingBasket {
+
+  ShoppingBasket basket;
+
+  @Before
+  public void setup() {
+    basket = new ShoppingBasket();
+  }
+
+}
---
+++ tests/TestShoppingBasket.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+/**
+ * Created by chris on 02/12/2017.
+ */
+public class TestShoppingBasket {
+
+
+
+}
